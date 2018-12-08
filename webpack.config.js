6a31890a396d5a64aed2ef70e562abf8d31f68f0
zakariaharti <zakariya.harti@gmail.com>
2018-12-08 11:50:45 +0100

    adde storybook

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const CleanPlugin = require('webpack-clean-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  devtool: 'source-map',
+  entry: {
+    main: './src/index.js'
+  },
+  resolve: {
+    extensions: [".js",".jsx"]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.bundle.js',
+    library: 'realert',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: 'node_modules',
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+          {
+            loader: 'eslint-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanPlugin(['build'])
+  ]
+}
