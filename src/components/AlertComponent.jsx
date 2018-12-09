23704972da1ba8e46c00b6df307f0ff4d10366a7
zakariaharti <zakariya.developer@gmail.com>
2018-12-09 19:29:06 +0100

    added AlertComponent

---
+++ src/components/AlertComponent.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+/* eslint-disable */
+
+const AlertComponent = (props) => {
+  const {
+    isOpen,
+    title,
+    content,
+    level,
+    icon,
+    allowHTML,
+    buttons,
+    dismissOnClickOutside,
+    dismissOnEsc,
+    dangerMode,
+    dismissAfter,
+
+  } = props;
+
+  return (
+    <div>gh</div>
+  );
+};
+
+export default AlertComponent;
