e22708bb69addd68047509900a4d77fbd3cf0a50
zakariaharti <zakariya.developer@gmail.com>
2018-12-14 12:34:44 +0100

    added Alert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,22 +1,100 @@
 import React, { Component } from 'react';
 
+import AlertComponent from './AlertComponent';
+
+/**
+ * Alert component class
+ *
+ * @author zakariaharti
+ */
 class Alert extends Component {
+  /**
+   * state of the component
+   */
   state = {
     isOpen: this.props.isOpen,
+    timer: setTimeout(() => {
+
+    }, this.props.dismissAfter),
   };
 
+  /**
+   * invoking onAdd with the component is mounted on the DOM
+   */
+  componentDidMount(){
+    const { onAdd } = this.props;
+    if(onAdd && typeof onAdd === "function"){
+      onAdd(this);
+    }
+  }
+
+  /**
+   * updating state based on props changes
+   */
   componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
+    const {
+      isOpen,
+      dismissAfter
+    } = this.props;
     if (prevProps.isOpen !== isOpen) {
       this.setState({ isOpen: isOpen });
+    }
+    if (prevProps.dismissAfter !== dismissAfter) {
+      this.setState({
+        timer: setTimeout(() => {
+
+         }, this.props.dismissAfter)
+      });
+    }
+  }
+
+  /**
+   * cleaning timeout
+   */
+  componentWillUnmount(){
+    clearTimeout(this.state.timer);
+
+    const { onRemove } = this.props;
+    if(onRemove && typeof onRemove === "function"){
+      onAdd(this);
+    }
+  }
+
+  /**
+   * close the alert
+   */
+  close = () = {
+    if(this.state.isOpen){
+      this.setState({ isOpen: false });
     }
   }
 
   render() {
+    const { isOpen } = this.state.isOpen;
+    const {
+      title,
+      content,
+      level,
+      icon,
+      allowHTML,
+      buttons,
+    } = this.props;
     return (
-      <p>gh</p>
+      <AlertComponent
+        isOpen={this.state.isOpen}
+        title={title}
+        content={content}
+        level={level}
+        icon={icon}
+        allowHTML={allowHTML}
+        buttons={buttons}
+      />
     );
   }
 }
+
+Alert.defaultProps = {
+  dismissAfter: 0
+};
 
 export default Alert;
