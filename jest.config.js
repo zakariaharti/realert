cbb5a8d61001834faac32a2630009aa4e51fdffa
zakariaharti <zakariya.developer@gmail.com>
2018-12-15 19:21:17 +0100

    added jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "testPathIgnorePatterns": [
+    "/node_modules/",
+    "/lib/"
+  ],
+  testMatch: null,
+  "testRegex": "(/test/.*|\\.(test|spec))\\.(js|jsx)$",
+  "moduleFileExtensions": [
+    "js",
+    "jsx"
+  ],
+  "coverageDirectory": "./coverage",
+  "setupTestFrameworkScriptFile": "<rootDir>test/init.js"
+};
